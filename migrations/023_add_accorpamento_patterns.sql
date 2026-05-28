da8d3dd87e88db16e3e5c8ea7b1bd219c0107bf1
Mirko D'Addiego <mirkodaddiego@gmail.com>
2026-05-28 23:45:24 +0200

    Fix: rimosso DROP COLUMN IF EXISTS in migrazione 023 per compatibilità MySQL 5.7

--- migrations/023_add_accorpamento_patterns.sql
+++ migrations/023_add_accorpamento_patterns.sql
@@ -1,8 +1,6 @@
 -- Migrazione: Aggiunta colonna accorpato_a per accorpamento pattern L1
 -- Data: 2026-05-28
 
-ALTER TABLE mapping_pendenze_pattern DROP COLUMN IF EXISTS accorpato_a;
-ALTER TABLE mapping_pendenze_pattern DROP INDEX IF EXISTS idx_accorpato;
 
 ALTER TABLE mapping_pendenze_pattern
   ADD COLUMN accorpato_a VARCHAR(35) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL AFTER cod_entrata;
