0d06640a81edba8cb06f8db21e82d5b5985ba207
mirkochipdotcom <mirko.daddiego@comune.montesilvano.pe.it>
2026-08-14 14:56:14 +0200

    feat(monitoring): forward Logger error/warning to Sentry

---
+++ tests/LoggerTest.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests;
+
+use App\Logger;
+use PHPUnit\Framework\TestCase;
+
+final class LoggerTest extends TestCase
+{
+    private static string $logFile;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$logFile = sys_get_temp_dir() . '/gil-logger-test-' . uniqid('', true) . '.log';
+        putenv('APP_LOG_PATH=' . self::$logFile);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        putenv('APP_LOG_PATH');
+        if (is_file(self::$logFile)) {
+            @unlink(self::$logFile);
+        }
+    }
+
+    public function testErrorWithoutExceptionWritesLogFile(): void
+    {
+        Logger::getInstance()->error('errore di test', ['foo' => 'bar']);
+        $this->assertStringContainsString('errore di test', (string)file_get_contents(self::$logFile));
+    }
+
+    public function testErrorWithExceptionDoesNotThrowAndWritesLogFile(): void
+    {
+        Logger::getInstance()->error('errore con eccezione', [], new \RuntimeException('boom'));
+        $this->assertStringContainsString('errore con eccezione', (string)file_get_contents(self::$logFile));
+    }
+
+    public function testWarningBackwardCompatibleTwoArgCall(): void
+    {
+        Logger::getInstance()->warning('warning di test');
+        $this->assertStringContainsString('warning di test', (string)file_get_contents(self::$logFile));
+    }
+}
